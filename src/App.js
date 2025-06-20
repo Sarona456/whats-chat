ea50028e230602edcffbe0ef07033a4cfa731173
YoussefMohamedCs <youssefmohamedcs1@gmail.com>
2025-06-20 17:56:20 +0300

    first

--- src/App.js
+++ src/App.js
@@ -2,22 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+   const contacts = [
+    { num: "201068683009", name: "Zainab OR Sara" },
+    { num: "201000102804", name: "Hana" },
+    { num: "201009796139", name: "Niema" },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Chat With Our Team</h1>
+      <div className="button-container">
+        {contacts.map(({ num, name }, i) => (
+          <a
+            key={i}
+            href={`https://wa.me/${num}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Chat with {name}
+          </a>
+        ))}
+      </div>
     </div>
   );
 }
